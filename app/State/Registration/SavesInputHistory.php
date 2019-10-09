a90587f3f0717edca51064702da8d425447d8c6a
Njenga Kabiru <njengakabiru@gmail.com>
2019-10-09 09:57:05 +0300

    Update the InputHistory trait with a default value
    Since input_history column doesn't have a default value, I add an
    empty array as a default. This will allow the simple concatenation
    of arrays to work.

--- app/State/Registration/SavesInputHistory.php
+++ app/State/Registration/SavesInputHistory.php
@@ -8,7 +8,7 @@ trait SavesInputHistory
 {
     protected function saveInputHistory(string $input)
     {
-        $inputHistory = $this->session->input_history;
+        $inputHistory = $this->session->input_history ?? [];
         $inputHistory += [static::class => $input];
 
         $this->session->input_history = $inputHistory;
