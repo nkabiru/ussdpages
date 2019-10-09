165aaf03a4ef3c87ba71765905aee1e2bd37f83f
Njenga Kabiru <njengakabiru@gmail.com>
2019-10-09 10:00:52 +0300

    Add functions for creating user & deleting the session in the CreateUser state

--- app/State/Registration/CreateUser.php
+++ app/State/Registration/CreateUser.php
@@ -2,6 +2,7 @@
 
 namespace App\State\Registration;
 
+use App\Jobs\CreateUserInDatabase;
 use App\UssdSession;
 
 class CreateUser implements State
@@ -20,12 +21,39 @@ class CreateUser implements State
 
     public function input($input): void
     {
-       dd($this->session->input_history);
+
     }
 
 
+    /**
+     *  Creating the user here because the input function won't be hit by the user.
+     *  Same goes for deleting the UssdSession.
+     *
+     * @return string
+     */
     public function view()
     {
+        $this->createUser();
+        $this->deleteUssdSession();
+
         return "END You have registered successfully. Please dial the shortcode again to login";
     }
+
+    protected function createUser(): void
+    {
+        $inputHistory = $this->session->input_history;
+
+        CreateUserInDatabase::dispatchNow([
+            'name' => $inputHistory[EnterName::class],
+            'pin' => $inputHistory[EnterPin::class],
+            'phone_number' => request('phoneNumber')
+        ]);
+    }
+
+    protected function deleteUssdSession()
+    {
+        $this->session->delete();
+    }
+
+
 }
